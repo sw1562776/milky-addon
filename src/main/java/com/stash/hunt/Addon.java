725dfb30369867970a1ce1445bbf72f87cb7692b
sw <sw1562776@gmail.com>
2025-07-31 15:00:02 -0700

    Update Addon.java

--- src/main/java/com/stash/hunt/Addon.java
+++ src/main/java/com/stash/hunt/Addon.java
@@ -2,7 +2,6 @@ package com.stash.hunt;
 
 import com.stash.hunt.hud.Weather;
 import com.stash.hunt.modules.*;
-import com.stash.hunt.modules.searcharea.SearchArea;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.settings.Settings;
@@ -24,57 +23,13 @@ public class Addon extends MeteorAddon {
     public void onInitialize() {
         LOG.info("Initializing Jefff Mod");
 
-        Modules.get().add(new SearchArea());
-        Modules.get().add(new AutoLogPlus());
-        Modules.get().add(new GotoPosition());
-        Modules.get().add(new ChestIndex());
-        Modules.get().add(new HighlightOldLava());
-        Modules.get().add(new VanityESP());
-        Modules.get().add(new AFKVanillaFly());
-        Modules.get().add(new AutoPortal());
         Modules.get().add(new AutoSnowman());
-        Modules.get().add(new Pitch40Util());
-//        Modules.get().add(new AutoTrade());
-//        Modules.get().add(new XPBot());
-//        Modules.get().add(new UnknownAccountNotifier());
-
-        Modules.get().add(new NoJumpDelay());
-        Modules.get().add(new GrimAirPlace());
-        Modules.get().add(new DiscordNotifs());
-//        Modules.get().add(new EndermanItemDetector());
-//        Modules.get().add(new GrimDuraFirework());
-//        Modules.get().add(new PacketTester());
-//        Modules.get().add(new StashMover2());
-//        Modules.get().add(new StashMoverListener());
-//        Modules.get().add(new PacketGrimFly());
-        Modules.get().add(new AutoEXPPlus());
-//        Modules.get().add(new LavaESP());
-
+        
         boolean baritoneLoaded = checkModLoaded("baritone", "baritone-meteor");
         boolean xaeroWorldMapLoaded = checkModLoaded("xaeroworldmap");
         boolean xaeroMinimapLoaded = checkModLoaded("xaerominimap");
         boolean xaeroPlusLoaded = checkModLoaded("xaeroplus");
-
-        if (xaeroWorldMapLoaded && xaeroPlusLoaded)
-        {
-//            Modules.get().add(new MudCracker());
-            if (xaeroMinimapLoaded)
-            {
-                Modules.get().add(new BetterStashFinder());
-                Modules.get().add(new OldChunkNotifier());
-            }
-            if (baritoneLoaded)
-            {
-                Modules.get().add(new TrailFollower());
-            }
-        }
-
-        if (baritoneLoaded)
-        {
-            Modules.get().add(new ElytraFlyPlusPlus());
-        }
-
-        Hud.get().register(Weather.INFO);
+        
     }
 
     @Override
