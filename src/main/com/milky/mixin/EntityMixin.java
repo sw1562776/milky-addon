a3d3233e6d813950646a5e1fd66fb12e15310096
sw <sw1562776@gmail.com>
2025-07-31 14:36:15 -0700

    Update EntityMixin.java

--- src/main/com/milky/mixin/EntityMixin.java
+++ src/main/com/milky/mixin/EntityMixin.java
@@ -1,6 +1,5 @@
-package com.stash.hunt.mixin;
+package com.milky.mixin;
 
-import com.stash.hunt.modules.ElytraFlyPlusPlus;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityPose;
@@ -14,39 +13,3 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.UUID;
 
 import static meteordevelopment.meteorclient.MeteorClient.mc;
-
-@Mixin(Entity.class)
-public class EntityMixin
-{
-    @Shadow
-    protected UUID uuid;
-
-    ElytraFlyPlusPlus efly = Modules.get().get(ElytraFlyPlusPlus.class);
-
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/entity/Entity;getPose()Lnet/minecraft/entity/EntityPose;", cancellable = true)
-    private void getPose(CallbackInfoReturnable<EntityPose> cir)
-    {
-        if (efly != null && efly.enabled() && this.uuid == mc.player.getUuid())
-        {
-            cir.setReturnValue(EntityPose.STANDING);
-        }
-    }
-
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/entity/Entity;isSprinting()Z", cancellable = true)
-    private void isSprinting(CallbackInfoReturnable<Boolean> cir)
-    {
-        if (efly != null && efly.enabled() && this.uuid == mc.player.getUuid())
-        {
-            cir.setReturnValue(true);
-        }
-    }
-
-    @Inject(at = @At("HEAD"), method = "pushAwayFrom", cancellable = true)
-    private void pushAwayFrom(Entity entity, CallbackInfo ci)
-    {
-        if (mc.player != null && this.uuid == mc.player.getUuid() && efly != null && efly.enabled() && !entity.getUuid().equals(this.uuid))
-        {
-            ci.cancel();
-        }
-    }
-}
