d96ecbaeb71d128a1e6130f8e0c4301fbdc5d4e6
sw <sw1562776@gmail.com>
2025-07-31 15:04:49 -0700

    Update KeyBindingMixin.java

--- src/main/java/com/stash/hunt/mixin/KeyBindingMixin.java
+++ src/main/java/com/stash/hunt/mixin/KeyBindingMixin.java
@@ -1,6 +1,5 @@
 package com.stash.hunt.mixin;
 
-import com.stash.hunt.modules.ElytraFlyPlusPlus;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.option.KeyBinding;
 import org.spongepowered.asm.mixin.Final;
@@ -21,16 +20,6 @@ public abstract class KeyBindingMixin {
     private String translationKey;
 
     @Unique
-    ElytraFlyPlusPlus efly = null;
 
     @Inject(at = @At("RETURN"), method = "isPressed", cancellable = true)
-    public void isPressed(CallbackInfoReturnable<Boolean> cir)
-    {
-        // setting it beforehand caused a crash because meteor wasnt loaded yet
-        efly = efly == null ? Modules.get().get(ElytraFlyPlusPlus.class) : efly;
-        if (efly != null && efly.isActive() && efly.enabled() && translationKey.equals("key.forward"))
-        {
-            cir.setReturnValue(true);
-        }
-    }
 }
