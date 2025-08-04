aa1184c7c3620667d85aa686e062b77f20dc6bbc
sw <sw1562776@gmail.com>
2025-08-03 20:43:12 -0700

    Update Addon.java

--- src/main/java/com/milky/hunt/Addon.java
+++ src/main/java/com/milky/hunt/Addon.java
@@ -24,6 +24,7 @@ public class Addon extends MeteorAddon {
 
         Modules.get().add(new AutoGolem());
         Modules.get().add(new AutoInvertedY());
+        Modules.get().add(new QuickCommand());
         
         boolean baritoneLoaded = checkModLoaded("baritone", "baritone-meteor");
         boolean xaeroWorldMapLoaded = checkModLoaded("xaeroworldmap");
