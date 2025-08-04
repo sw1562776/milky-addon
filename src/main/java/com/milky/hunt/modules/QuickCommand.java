694125e2cc40f181ad90ab39f771aba79d0ca5d8
sw <sw1562776@gmail.com>
2025-08-04 00:19:58 -0700

    Update QuickCommand.java

--- src/main/java/com/milky/hunt/modules/QuickCommand.java
+++ src/main/java/com/milky/hunt/modules/QuickCommand.java
@@ -13,13 +13,9 @@ import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.registry.Registries;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BarrelBlockEntity;
-import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.block.entity.ShulkerBoxBlockEntity;
-import net.minecraft.block.entity.HopperBlockEntity;
-import net.minecraft.block.entity.DispenserBlockEntity;
-import net.minecraft.block.entity.DropperBlockEntity;
+import net.minecraft.block.entity.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.world.chunk.WorldChunk;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -107,20 +103,19 @@ public class QuickCommand extends Module {
 
         String nearbyPlayers = String.join(", ", nearbyNames);
 
-        int containerCount = 0;
-for (BlockEntity be : mc.world.chunkManager.getLoadedChunks()
-    .stream()
-    .flatMap(chunk -> chunk.getBlockEntities().values().stream())
-    .toList()) {
-    if (be instanceof ChestBlockEntity
-        || be instanceof BarrelBlockEntity
-        || be instanceof ShulkerBoxBlockEntity
-        || be instanceof HopperBlockEntity
-        || be instanceof DispenserBlockEntity
-        || be instanceof DropperBlockEntity) {
-        containerCount++;
-    }
-}
+         int containerCount = 0;
+        for (WorldChunk chunk : ((ClientWorld) mc.world).getChunkManager().getChunks()) {
+            for (BlockEntity be : chunk.getBlockEntities().values()) {
+                if (be instanceof ChestBlockEntity
+                    || be instanceof BarrelBlockEntity
+                    || be instanceof ShulkerBoxBlockEntity
+                    || be instanceof HopperBlockEntity
+                    || be instanceof DispenserBlockEntity
+                    || be instanceof DropperBlockEntity) {
+                    containerCount++;
+                }
+            }
+        }
 
 
         String result = input
