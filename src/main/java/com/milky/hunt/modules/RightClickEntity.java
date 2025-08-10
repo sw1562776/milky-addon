85d6e1415a4962bb0248c01984a2191fab95f81e
sw <sw1562776@gmail.com>
2025-08-10 16:18:03 -0700

    Update RightClickEntity.java

--- src/main/java/com/milky/hunt/modules/RightClickEntity.java
+++ src/main/java/com/milky/hunt/modules/RightClickEntity.java
@@ -20,6 +20,9 @@ import java.util.List;
 import java.util.Set;
 
 public class RightClickEntity extends Module {
+    
+    public static boolean isInteracting = false;
+    
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Boolean> oneInteractionPerTick = sgGeneral.add(new BoolSetting.Builder()
@@ -84,6 +87,9 @@ public class RightClickEntity extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
+
+        isInteracting = false;
+        
         if (mc.world == null || mc.player == null) return;
 
         if (oneTime.get()) {
@@ -103,6 +109,8 @@ public class RightClickEntity extends Module {
                 || (ignoreBabies.get() && ((LivingEntity) entity).isBaby())) continue;
 
             if (oneTime.get()) used.add(entity);
+
+             isInteracting = true;
 
             Rotations.rotate(Rotations.getYaw(entity), Rotations.getPitch(entity), () -> {
                 sendInteractPackets(entity);
